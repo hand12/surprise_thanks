1f0b6aba008b4d9caf267c990ababfbeef3afd64
yusuke yamashita <yusuke@yusukenoMacBook-Pro.local>
2017-11-01 02:58:36 +0900

    add user profile image form

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -22,3 +22,28 @@ function previewFile() {
 		reader.readAsDataURL(file);
 	}
 };
+
+function previewProfileImageFile() {
+	var preview = document.createElement('img');
+	var image_field = document.getElementById('profile_image');
+
+	var exsist_preview_image = document.getElementById('preview_image');
+
+	if (exsist_preview_image) {
+		exsist_preview_image.parentNode.removeChild(exsist_preview_image);
+	}
+	
+	var file = document.querySelector('input[type=file]').files[0];
+	var reader = new FileReader();
+
+	reader.addEventListener("load", function() {
+		preview.src = reader.result;
+		preview.setAttribute('id', 'preview_image');
+		image_field.style.display = 'block';
+		image_field.appendChild(preview);
+	}, false);
+
+	if (file) {
+		reader.readAsDataURL(file);
+	}
+}
