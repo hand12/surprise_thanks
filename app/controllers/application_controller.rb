f1071bf8db63806d79a90955b3e96def171d7aee
yusuke yamashita <yusuke@yusukenoMacBook-Pro.local>
2017-11-02 02:35:19 +0900

    add flash message animation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def configure_permitted_parameters
+		added_attrs = [:name, :handle_name, :profile_image ]
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+	end
 end
